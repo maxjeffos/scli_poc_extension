dff049266b9d1d5320d2028718cddcda9f3f11a6
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2022-05-12 09:53:50 -0400

    refactor to use stdin message

--- main.go
+++ main.go
@@ -1,13 +1,77 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
 )
-import flag "github.com/spf13/pflag"
 
 func main() {
-	lang := flag.StringP("lang", "l", "en", "the language")
-	flag.Parse()
+	inputString, err := ReadInput()
+	if err != nil {
+		fmt.Println("failed reading input")
+		panic(err)
+	}
 
-	fmt.Println("Woof in", *lang)
+	input, err := ParseInput[Input](inputString)
+	if err != nil {
+		fmt.Println("failed parsing input JSON to struct")
+		panic(err)
+	}
+
+	lang := input.Lang
+	fmt.Println("Woof in", lang)
+}
+
+type Input struct {
+	Lang string `json:"lang"`
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// The stuff below should go in the extensions library
+///////////////////////////////////////////////////////////////////////////////
+
+func GetInputArgs() *Input {
+	var args Input
+	args.Lang = "en"
+	return &args
+}
+
+func ReadInput() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	consecutiveNewlinesCount := 0
+	chars := []rune{}
+
+	for {
+		char, _, err := reader.ReadRune()
+		if err != nil {
+			return "", nil
+		}
+		if char == '\n' {
+			consecutiveNewlinesCount++
+		} else {
+			consecutiveNewlinesCount = 0
+			chars = append(chars, char)
+		}
+
+		if consecutiveNewlinesCount == 2 {
+			break
+		}
+	}
+
+	inputString := string(chars)
+	return inputString, nil
+}
+
+func ParseInput[T any](inputString string) (*T, error) {
+	rawBytes := []byte(inputString)
+
+	var input T
+	err := json.Unmarshal(rawBytes, &input)
+	if err != nil {
+		return nil, err
+	}
+
+	return &input, nil
 }
